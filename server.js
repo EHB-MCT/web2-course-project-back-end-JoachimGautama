f42057a65660fef26d337e32e8f34db2f6e73d32
JoachimGautama <joachim.gautama@student.ehb.be>
2025-11-26 18:42:50 +0100

    dummy build to satisfy the render demons

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { loadEnvFile } from "process";
+import cors from "cors";
+const app = express();
+loadEnvFile();
+const PORT = process.env.PORT;
+
+app.use(express.json(), cors());
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
