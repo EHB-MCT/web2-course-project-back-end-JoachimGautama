096e88da856ad339ff068ab15ff56c0dcf285e52
JoachimGautama <joachim.gautama@student.ehb.be>
2025-12-17 23:34:21 +0100

    Squashed commit of the following:
    commit 97c8b3c746a580235d9bee7d7a15c23417f179f1
    Author: JoachimGautama <joachim.gautama@student.ehb.be>
    Date:   Wed Dec 17 17:17:33 2025 +0100
    
        removed unecessary code
    
    commit c1eae662192f9734e21456ae9bd7d17c2b70d7bf
    Author: JoachimGautama <joachim.gautama@student.ehb.be>
    Date:   Wed Dec 17 17:14:33 2025 +0100
    
        updated README
    
    commit e1a2a3ba85684cc2606cbdee9ab1aeadd3e815f1
    Author: JoachimGautama <joachim.gautama@student.ehb.be>
    Date:   Wed Dec 17 16:51:31 2025 +0100
    
        update README
    
    commit 2aae56c69aad062ac28cfa72c7c6ec49efe61152
    Author: JoachimGautama <joachim.gautama@student.ehb.be>
    Date:   Wed Dec 17 16:48:26 2025 +0100
    
        created mongodb connection
    
    commit 407edaf1bf9d7251d3bcf14f57378b9b086d9f90
    Author: JoachimGautama <joachim.gautama@student.ehb.be>
    Date:   Wed Nov 26 20:06:21 2025 +0100
    
        update README in case I forget the command

--- README.md
+++ README.md
@@ -16,8 +16,13 @@ I recommend checking the status or waking up the server by clicking the live on 
 
 List **ALL your used sources** here:
 
-- Used a tutorial or example code? Place a link to them here. (youtube, website, github, ...) and **list the files in which they were used**.
+-
 
 It's not enough to only list the sources here, also add the sources to the top of the files in which you used them and indicate the specific parts of the code which are not your own (by writing comments and refering to the correct source(s)). \
 IF you use a source as the base/start for a file, then first place the source in the file and **COMMIT it IMMEDIATELY, BEFORE** adding your own code or modifying it. \
 **ALWAYS** use concise BUT CLEAR commit messages!
+
+## note to self
+
+`git push origin testInProd:main`  
+best command ever
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { loadEnvFile } from "process";
 import cors from "cors";
+import { MongoClient } from "mongodb";
 
 const app = express();
 try {
@@ -10,9 +11,15 @@ try {
 }
 
 const PORT = process.env.PORT || 3000;
-const TEST = process.env.TEST || "no env present";
+const PASSWORD = process.env.PASSWORD;
+const NAME = process.env.NAME;
+const HOST = process.env.HOST;
+const APP = process.env.APP;
 
-app.use(express.json(), cors());
+const url = `mongodb+srv://${NAME}:${PASSWORD}@${HOST}/${APP}?appName=characters`;
+const client = new MongoClient(url);
+
+app.use(express.json(), cors(), express.urlencoded({ extended: true }));
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello World" });
@@ -21,6 +28,4 @@ app.get("/api/hello", (req, res) => {
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
-  console.log("test commit");
-  console.log(TEST);
 });
